9c892b4f66150f4a8d893175b25f2158b633cf89
Benjymack <newzealandbenjy@gmail.com>
2022-06-30 18:36:24 +1200

    Created footer and added to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
     import '../app.css';
     import {page_name} from '../stores.ts';
 </script>
 
 <Header {page_name} />
 
-<main>
-    <slot />
-</main>
+<slot />
+
+<Footer />
