5aeac278d019def12024587ef7bad390771f9c52
Benjymack <newzealandbenjy@gmail.com>
2022-07-02 20:16:47 +1200

    Added initial searchbox and "Search through ... reactions from ... sources" text

---
+++ src/lib/searchbox/SearchBox.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+    import SubstanceSearch from "./SubstanceSearch.svelte";
+    import ReactionSearch from "./ReactionSearch.svelte";
+    import RetrosynthesisSearch from "./RetrosynthesisSearch.svelte";
+
+    enum SearchType {
+        Substance = SubstanceSearch,
+        Reaction = ReactionSearch,
+        Retrosynthesis = RetrosynthesisSearch,
+    }
+
+    let search_type: SearchType = SearchType.Substance;
+</script>
+
+<div id="search-box">
+    <div id="options">
+        <button class="option" on:click={() => search_type = SearchType.Substance}>Substance</button>
+        <button class="option" on:click={() => search_type = SearchType.Reaction}>Reaction</button>
+        <button class="option" on:click={() => search_type = SearchType.Retrosynthesis}>Retrosynthesis</button>
+    </div>
+
+    <svelte:component this={search_type} />
+
+    <button id="search-button">Search</button>
+</div>
+
+<style>
+    #search-box {
+        border: var(--medium-grey) 1px solid;
+        width: 50vw;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        background: var(--light-grey);
+
+        --border-radius: 10px;
+        border-radius: var(--border-radius);
+    }
+
+    #search-button {
+        background-color: var(--dark-accent-color);
+        color: var(--light-text-color);
+    }
+
+    #options {
+        display: flex;
+
+        --option-border-width: 1px;
+
+        width: calc(100% + 2 * var(--option-border-width));
+
+        margin-top: calc(-1 * var(--option-border-width));
+    }
+
+    .option {
+        width: 100%;
+        text-align: center;
+        background-color: var(--light-background-color);
+
+        border: var(--dark-grey) solid;
+        border-bottom-width: var(--option-border-width);
+        border-top-width: var(--option-border-width);
+        border-left-width: 0;
+    }
+
+    .option {
+        border-right-width: var(--option-border-width);
+    }
+
+    .option:first-child {
+        border-left-width: var(--option-border-width);
+
+        border-top-left-radius: var(--border-radius);
+    }
+
+    .option:last-child {
+        border-top-right-radius: var(--border-radius);
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
     import {page_name} from "../stores.ts";
+    import SearchBox from "$lib/searchbox/SearchBox.svelte";
 
     page_name.set("Home");
+
+    let num_reactions = 1234;
+    let num_sources = 321;
 </script>
+
+<main>
+    <p id="summary">Search through {num_reactions} reactions from {num_sources} sources.</p>
+
+    <SearchBox />
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
