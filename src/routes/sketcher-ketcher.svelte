d4b24b7b62580d7fb4db0139892e65b8844d3b50
Benjymack <newzealandbenjy@gmail.com>
2022-08-19 17:19:16 +1200

    Added Ketcher and ChemDoodle example pages

---
+++ src/routes/sketcher-ketcher.svelte
@@ -0,0 +1,10 @@
+<main>
+    <iframe id="ifKetcher" src="ketcher/index.html" width="800" height="600"></iframe>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+    }
+</style>
