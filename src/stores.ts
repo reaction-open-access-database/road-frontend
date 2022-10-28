d8741d735b9e56a0ab90c2383ac3a48cb2a43042
Benjy <newzealandbenjy@gmail.com>
2022-10-28 19:08:29 +1300

    Updated API URL

--- src/stores.ts
+++ src/stores.ts
@@ -2,7 +2,7 @@ import { writable } from "svelte/store";
 import { browser } from "$app/env";
 
 export const page_name = writable("");
-export const API_URL = "http://road.ddns.net:8000";
+export const API_URL = "https://road.ddns.net:8001";
 
 const initial_user = browser ? localStorage.getItem("user") : null;
 
