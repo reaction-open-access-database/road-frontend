03f6f2f8e021e0b80b876b8d8f58c4747649c896
Benjy <newzealandbenjy@gmail.com>
2023-01-09 19:57:07 +1300

    Removed unnecessary code

--- src/lib/searchresults/SearchResult.svelte
+++ src/lib/searchresults/SearchResult.svelte
@@ -1,37 +1,26 @@
-<script context="module">
-    let molecule_options = {
-        terminalCarbons: true,
-        padding: 1,
-    };
-    let reaction_options = {};
-
-    let current_molecule_id = 0;
-</script>
-
 <script lang="ts">
     import { onMount } from "svelte";
     import type { Molecule } from "../../types";
 
     export let result: Molecule;
 
+    const molecule_options = {
+        terminalCarbons: true,
+        padding: 1,
+    };
+
     let molecule_structure;
 
     onMount(() => {
-        // let sd = new SmilesDrawer.Drawer(molecule_options);
-        // SmilesDrawer.parse(result.smiles, (tree) => {
-        //     sd.draw(tree, molecule_structure);
-        // });
         let sd = new SmiDrawer(molecule_options);
         sd.drawMolecule(result.smiles, 'svg', 'light', (svg) => {
             sd.svgToCanvas(svg, molecule_structure);
         });
-        // let sd = new SmiDrawer(molecule_options, reaction_options);
-        // sd.draw(result.smiles, '#' + molecule_structure.id);
     })
 </script>
 
 <div class="search-result">
-    <canvas id={'molecule-structure-' + current_molecule_id++} class="molecule-structure" bind:this={molecule_structure}></canvas>
+    <canvas class="molecule-structure" bind:this={molecule_structure}></canvas>
     <table class="other-info">
         <tr>
             <td>Name</td>
